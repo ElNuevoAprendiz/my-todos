a6658349c4e7f4cff0dc6a0592f33a9f509319c6
Dany <profedanieljuan@gmail.com>
2025-11-24 18:22:39 -0300

    Se comienza con el desarrollo de la interfase.
    Se agrega un componente Textinput y un boton personalizado utilizando el
    componente TouchableOpacity, se agregan los respectivos estilos, se
    ajusta el espaciado entre componentes usando la propiedad
    justifyContent: 'space-between', se configura la pressntacion de los
    componentes con la propiedad flexDirection: 'row' todo esto en el view
    que los contiene y que tiene el estilo inputContainer.
    
    Previamente se ajusto el tamaño de los componentes importando el objeto
    Dimension de java y usando su metodo get para obtener el tamaño de la
    pantalla. y se uso esto junto con la propiedad width en los dos
    componentes.

--- App.tsx
+++ App.tsx
@@ -1,30 +1,65 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Hola!
+      <Text style={styles.title}>
+        Mis tareas
       </Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Agregar una nueva tarea' style={styles.textInput} />
+        <TouchableOpacity style={styles.addButton}>
+          <Text style={styles.whiteText}>Agregar</Text>
+        </TouchableOpacity>
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
-    backgroundColor: "#aaffaa",
-    
+    backgroundColor: "#fff",
     width: "100%",
-    
     borderRadius: 10,
     marginTop: 50,
     padding: 10,
+  },
+  inputContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+  },
+  title: {
+    fontSize: 20,
+    color: "#6f6f6f",
+  },
+  whiteText: {
+    fontSize: 18,
+    color: "#FFF",
   },
   text: {
     fontStyle: "italic",
     fontSize: 18,
   },
+  textInput: {
+    borderColor: "#6f6f6f",
+    borderWidth: 1,
+    width: Dimensions.get('screen').width * 0.6,
+    borderRadius: 15,
+    marginLeft: 15,
+    
+    
+
+  },
+  addButton: {
+    backgroundColor: '#5897fb',
+    width: Dimensions.get('screen').width * 0.25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+
+  }
 });
