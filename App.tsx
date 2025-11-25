d059c286f59173d34901309d64c6e78181ac93e1
Dany <profedanieljuan@gmail.com>
2025-11-24 21:08:37 -0300

    Se agrega la visualizacion de las tareas.
    Se utiliza el componente FlatList de React y se crean la funcion render item para mostrar las tareas jjuntos con un array de las tareas. La tareas utilizan un prototipo llamado Task para ser pasadas como un objeto a la funcion y luego se imprime la propiedad title del objeto. Tambien se usa un view para mostrar las tareas y asi poder definirle un estilo, el cual se llama scrollContainer

--- App.tsx
+++ App.tsx
@@ -1,7 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions, FlatList } from "react-native";
+
+const tasks=[
+  {
+    title:'Alimentar al perro',
+    done: false,
+    date: new Date(),
+},
+{
+    title:'Salir a correr',
+    done: false,
+    date: new Date(),
+},
+]
+
+interface Task {
+  title: string;
+  done: boolean;
+  date: Date;
+} 
 
 export default function App() {
+  function renderItem({item}: {item: Task}) {
+    return(
+      <View style={styles.itemsContainer}>
+        <Text style={styles.text }>{item.title}</Text>)
+      </View>);  
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -13,8 +38,14 @@ export default function App() {
           <Text style={styles.whiteText}>Agregar</Text>
         </TouchableOpacity>
       </View>
-      
+      <View style={styles.scrollContainer}>
+        <FlatList 
+          renderItem={renderItem}
+          data={tasks}
+        />
+      </View>
     </View>
+    
   );
 }
 
@@ -50,9 +81,6 @@ const styles = StyleSheet.create({
     width: Dimensions.get('screen').width * 0.6,
     borderRadius: 15,
     marginLeft: 15,
-    
-    
-
   },
   addButton: {
     backgroundColor: '#5897fb',
@@ -60,6 +88,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10,
+  },
+  scrollContainer: {
 
-  }
+  },
+  itemsContainer: {
+    paddingVertical: 20,
+    borderBottomColor: '#e4e4e4',
+    borderBottomWidth: 1,
+  },  
 });
