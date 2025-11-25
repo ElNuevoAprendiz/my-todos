dfcb1cb29e572369c76e46af40555b495cbb5b86
ElNuevoAprendiz! <profedanieljuan@gmail.com>
2025-11-25 08:31:30 -0300

    Sincronizacion con notebook

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { StatusBar } from "expo-status-bar";
+
+import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions, FlatList } from "react-native";
 
 const tasks=[
